83e640784e7cc3daa9eab27d95f060747841c123
Benjamin Müller <benjamin@urbn.de>
2013-05-07 19:54:30 +0200

    Initial Commit

---
+++ BMExtPageControllerExampleIOS/BMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BMAppDelegate.h
+//  BMExtPageControllerExampleIOS
+//
+//  Created by Benjamin Müller on 07.05.13.
+//  Copyright (c) 2013 BM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BMExtPageControllerExampleIOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BMExtPageControllerExampleIOS
+//
+//  Created by Benjamin Müller on 07.05.13.
+//  Copyright (c) 2013 BM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BMAppDelegate class]));
+    }
+}
